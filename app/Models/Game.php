291b0d2e7989bfbdb22ac7c2d34e852664fff859
Luigi Minardi <lugminsinlag@gmail.com>
2022-04-07 13:30:48 +0200

    feat: Creating the Game Model.

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'title',
+        'thumbnail_url',
+        'description',
+        'url',
+        'date',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+
+    protected $casts = [
+        'date' => 'date',
+    ];
+
+    public function parties()
+    {
+        return $this->hasMany(Party::class, 'game_id');
+    }
 }
