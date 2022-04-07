ef538b14c2c41bed20a913a8893acb1137a51209
Luigi Minardi <lugminsinlag@gmail.com>
2022-04-07 15:22:55 +0200

    fix: Solving bug at user migration and rennaming it to be more understandable.

--- database/migrations/2022_04_07_114027_update_users_table.php
+++ database/migrations/2022_04_07_114027_update_users_table.php
@@ -13,15 +13,8 @@ class UserMigration extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+        Schema::table('users', function (Blueprint $table) {
             $table->string('nickname');
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
