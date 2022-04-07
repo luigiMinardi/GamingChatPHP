5832781724c98223f7b5c8f16d75b7b893f5a86a
Luigi Minardi <lugminsinlag@gmail.com>
2022-04-07 13:31:46 +0200

    feat: Creating the Message Model.

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'message',
+        'date',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'user_id',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+
+    protected $casts = [
+        'date' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
